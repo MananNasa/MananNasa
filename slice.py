bd3f861ec5e2dd4fcb0242c650909a182a737bda
MananNasa <144117171+MananNasa@users.noreply.github.com>
2024-11-01 19:54:07 +0530

    Create slice.py
    it is the basic python codes for slicing

---
+++ slice.py
@@ -0,0 +1,18 @@
+b = [0, 1, 4, 6, 9]
+print(b[3:-1:1])
+print(b[2:-2:2])
+print(b[1:-1])
+print(b[-2:-1])
+print(b[0:1:5])
+print(b[-1:-4:-1])
+print(b[2:-2:1])
+print(b[-3:-1])
+print(b[-3:-1:2])
+print(b[4:-1:1])
+
+#STRING
+s = 'PYTHON'
+print(s[0:3])
+print(s[-6:-3])
+print(s[12:1])
+print(s[100:-2])
